27a55ce4c32ffb3d0243e2f3b9dfa032f392a721
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-01 21:06:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(listen)
+      listen.select do |anag|
+      (@word.split("").sort) == (anag.split("").sort)
+    # binding.pry 
+    end
+  end
+end 
+
