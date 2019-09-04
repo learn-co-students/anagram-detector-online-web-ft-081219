d5487e21ff714ec611c9c04423362c72ed77b0f5
Matthew Ream <matthewtimothyream@gmail.com>
2019-09-04 16:35:56 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+attr_accessor :possibility
+
+def initialize(word)
+@possibility = word
+end
+
+def match(array)
+    matches = []
+    array.each do |item|
+   if item.split("").sort == @possibility.split("").sort
+    matches << item
+    end
+    end
+    matches
+end
+end
